ec6d199799765e7498e2af8d813f561dacca5454
shreyasharmuh <deadpoison75@gmail.com>
2026-06-17 16:18:37 +0530

    final update

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
   </Link>
 
   <Link href="/prostituee">
-    PROSTITUTÉE
+    PROSTITUÉE
   </Link>
 
   <Link href="/collaborations">
--- src/components/prostituee/Hero.tsx
+++ src/components/prostituee/Hero.tsx
@@ -49,7 +49,7 @@ export default function Hero() {
             "
             >
 
-              PROSTIUTÉE
+              PROSTITUÉE
 
             </h1>
 
